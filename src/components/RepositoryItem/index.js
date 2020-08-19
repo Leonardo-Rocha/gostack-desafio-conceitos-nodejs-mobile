df51899a471d80b7185223c181ca357c2afdf5b4
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-08-19 15:43:39 -0400

    Change api service baseURL Add component RepositoryItem with styles Change app to load repositories from API and render with a FlatList of RepositoryItem

---
+++ src/components/RepositoryItem/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { TouchableOpacity, View, Text, FlatList } from 'react-native';
+
+import api from '../../services/api';
+
+import styles from './styles';
+
+export default function RepositoryItem({ repository }) {
+  const [likes, setLikes] = useState(repository.likes)
+
+  async function handleLikeRepository(id) {
+    api.post(`/repositories/${id}/like`).then(response => {
+      setLikes(response.data);
+    });
+  }
+
+  return (
+    <View style={styles.repositoryContainer}>
+      <Text style={styles.repository}>{repository.title}</Text>
+
+      <View style={styles.techsContainer}>
+        <FlatList 
+          horizontal={true}
+          data={repository.techs}
+          keyExtractor={tech => tech}
+          renderItem={({item: tech}) => (
+            <Text style={styles.tech}>{tech}</Text>
+          )}
+        />
+      </View>
+
+      <View style={styles.likesContainer}>
+        <Text
+          style={styles.likeText}
+          testID={`repository-likes-${repository.id}`}
+        >
+          {likes} {likes !== 1 ? 'curtidas' : 'curtida'}
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleLikeRepository(repository.id)}
+        testID={`like-button-${repository.id}`}
+      >
+        <Text style={styles.buttonText}>Curtir</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
